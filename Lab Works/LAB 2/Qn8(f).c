00d5e359374b6e5b7ab9ed708cc5e3e88bfe06c4
Rebika Parajuli <pas081bct027@wrc.edu.np>
2024-12-17 18:20:06 +0545

    Add files via upload

---
+++ Lab Works/LAB 2/Qn8(f).c
@@ -0,0 +1,13 @@
+//qn 8 (c)
+//wap to y=x>>z+z<<1(x=2,z=4)
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int x=2, z=4; 
+printf ("%d ",x>>z+z<<1);
+printf("%d",~x);
+	return 0;
+	getch();
+}
