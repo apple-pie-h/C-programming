229785468bb8e0ca4d762ba05cb31baf770412f9
Rebika Parajuli <pas081bct027@wrc.edu.np>
2024-12-23 17:11:19 +0545

    Update Qn7.c

--- Lab Works/LAB 2/Qn7.c
+++ Lab Works/LAB 2/Qn7.c
@@ -10,7 +10,7 @@ printf("Size of char: %lu bytes\n", sizeof(char));
 printf("Size of int: %lu bytes\n", sizeof(int));
 printf("Size of float: %lu bytes\n", sizeof(float));
 printf("Size of double: %lu bytes\n", sizeof(double));
-printf("Size of pointer: %lu bytes\n", sizeof(long double));
+printf("Size of long double: %lu bytes\n", sizeof(long double));
 return 0;
 	getch();
 }
