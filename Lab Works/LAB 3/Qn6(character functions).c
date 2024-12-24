7f1792e2739cf6522b3e48098d7c93e30c14bf75
Rebika Parajuli <pas081bct027@wrc.edu.np>
2024-12-24 09:07:07 +0545

    Create Qn6(character functions).c

---
+++ Lab Works/LAB 3/Qn6(character functions).c
@@ -0,0 +1,20 @@
+/*Write a program to take a character input from keyboard and check if 
+the character is using character functions below: 
+a) Alphanumeric (isalnum) 
+b) Blank character (isblank) 
+c) Alphabetic (isalpha) 
+d) Control character (iscntrl) 
+e) Number-digit (isdigit) 
+f) Upper case (isupper) 
+g) Lower case (islower) 
+h) Hexadecimal digit (ixdigit) 
+i) Graphical character (isgraph) 
+*/
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  getch();
+  return 0;
+}
