a302f1eb60cde6e22710d01a82d22043beef7ae8
Rebika Parajuli <pas081bct027@wrc.edu.np>
2024-12-24 09:06:02 +0545

    Create Qn6(ASCII code).c

---
+++ Lab Works/LAB 3/Qn6(ASCII code).c
@@ -0,0 +1,10 @@
+/*Write a program to take a character input from keyboard and check if it is a 
+number or alphabet or special character using ASCII CODE. */
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  getch();
+  return 0;
+}
