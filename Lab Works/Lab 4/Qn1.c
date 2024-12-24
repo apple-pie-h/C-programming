a9739094c08405092aa82ba93379d2b639bf4931
Rebika Parajuli <pas081bct027@wrc.edu.np>
2024-12-24 09:08:34 +0545

    Create Qn1.c

---
+++ Lab Works/Lab 4/Qn1.c
@@ -0,0 +1,15 @@
+/* Write a program to input marks of 5 subjects (Physics, Chemistry, 
+Math, English& Biology) for a student. Display the rank of each 
+subjects and also the result of total marks and percentage obtained 
+with his/her rank in the class. The rank is categorized as fail (marks 
+< 40%), pass & third division (marks between 40 to 55%), second 
+(marks between 55 to 65%), first (marks between 65 to 80%), 
+Distinction (marks between 80 to 95%), extra ordinary (marks 
+above 95 to 100%).*/
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  getch();
+  return 0;
+}
