00ef3ee74c392fc43d3f4cd979d66d7caf651f57
Rebika Parajuli <pas081bct027@wrc.edu.np>
2024-12-29 17:35:51 +0545

    Update Qn6(ASCII code).c

--- Lab Works/LAB 3/Qn6(ASCII code).c
+++ Lab Works/LAB 3/Qn6(ASCII code).c
@@ -5,6 +5,25 @@ number or alphabet or special character using ASCII CODE. */
 #include<conio.h>
 int main()
 {
+  char a;
+  int b;
+  printf("Enter a character");
+  scanf("%c",&a);
+  b=a;
+  if (b>=48 && b<=57)
+  {
+    printf("Number");
+  }
+  else if (b>=33 && b<=47 || b>=58 && b<=64)
+  {
+    printf("Special Characters");
+  }
+  else if (b>=65 && b<=90 || b>=97 && b<=122)
+  {
+    printf("Alphabets");
+  }
+  else
+  printf("Error");
   getch();
   return 0;
 }
