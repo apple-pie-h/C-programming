811c07a26410ec16084ed8aab62a6aac5b1cb81d
Rebika Parajuli <pas081bct027@wrc.edu.np>
2024-12-30 16:51:02 +0545

    Update Qn1(a).c

--- Lab Works/LAB 3/Qn1(a).c
+++ Lab Works/LAB 3/Qn1(a).c
@@ -6,28 +6,28 @@
 #define divide /
 int main()
 {
-	float x,y;
-	char ch1;
+	float x,y,ch1;
+	char ch2;
 	printf("Enter two numbers");
 	scanf("%f %f",&x,&y);
 	printf("Enter (+,-,*,/)");
-	ch1= getch();
-	if (ch1=='+')
+	ch2= getch();
+	if (ch2=='+')
 	{
 		ch1= x plus y;
 			printf("Sum is %d",ch1);
 	}
-	else if (ch1=='-')
+	else if (ch2=='-')
 	{
 		ch1= x minus y;
 			printf("difference is %d",ch1);
 	}
-	else if (ch1=='*')
+	else if (ch2=='*')
 	{
 		ch1= x multiply y;
 			printf("product is %d",ch1);
 	}
-	else if (ch1=='/')
+	else if (ch2=='/')
 	{
 		ch1= x divide y;
 			printf("division is %d",ch1);
@@ -37,7 +37,7 @@ int main()
 		printf("Error");
 	}
 	/*
-	switch(ch1)
+	switch(ch2)
 	{
 		case 'x':
 			{
