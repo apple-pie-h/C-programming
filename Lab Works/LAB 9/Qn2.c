437b2b798e02633387635471990506fb97b97bdd
abc <pas081bct027@wrc.edu.np>
2025-02-03 21:13:05 +0545

    Create Qn2.c

---
+++ Lab Works/LAB 9/Qn2.c
@@ -0,0 +1,5 @@
+// Create a structure named company which has name, address, phone and
+// noOfEmployee as member variables. Read name of company, its address,
+// phone and noOfEmployee. Finally display these members’ value.
+
+
