a8dba6d54b296bda8bb34dd78c14a3d9151937a3
abc <pas081bct027@wrc.edu.np>
2025-02-17 18:28:52 +0545

    Update Qn2.c

--- Lab Works/LAB 9/Qn2.c
+++ Lab Works/LAB 9/Qn2.c
@@ -3,3 +3,38 @@
 // phone and noOfEmployee. Finally display these members’ value.
 
 
+#include <stdio.h>
+#include <string.h> // Include for string functions like strcpy
+
+// Structure definition
+struct company {
+    char name[100];
+    char address[100];
+    char phone[20];
+    int noOfEmployee;
+};
+
+int main() {
+    struct company myCompany;
+
+    printf("Enter company name: ");
+ scanf("%s", myCompany.name);
+
+    printf("Enter company address: ");
+    scanf("%s", myCompany.address);
+
+    printf("Enter company phone number: ");
+    scanf("%s", myCompany.phone);
+
+    printf("Enter number of employees: ");
+    scanf("%d", &myCompany.noOfEmployee);
+
+
+    printf("\nCompany Information:\n");
+    printf("Name: %s\n", myCompany.name);
+    printf("Address: %s\n", myCompany.address);
+    printf("Phone: %s\n", myCompany.phone);
+    printf("Number of Employees: %d\n", myCompany.noOfEmployee);
+
+    return 0;
+}
